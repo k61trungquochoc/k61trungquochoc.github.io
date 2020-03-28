db24cebda42a4598beb1f97409fd450b00168cf8
Herry Vinh <hongvinhxc@gmail.com>
2020-03-28 19:45:03 +0700

    Updates

---
+++ precache-manifest.332e5d071580c382263e533caa514fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b51e68260f934b7756a9dbac7e8ef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec78d1097d968e5f37ef",
+    "url": "/static/css/main.93cac436.chunk.css"
+  },
+  {
+    "revision": "16b80cb0f6750f909bc4",
+    "url": "/static/js/2.905de649.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.905de649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec78d1097d968e5f37ef",
+    "url": "/static/js/main.95855549.chunk.js"
+  },
+  {
+    "revision": "7a58f009ec5c19bda80d",
+    "url": "/static/js/runtime-main.6e48cc6b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
